578e860037f8527cd1b80f74c6b06a9efc86e34b
Fabio Rueda <avances123@gmail.com>
2020-04-10 09:27:54 +0200

    compila

---
+++ Ejemplo1.cpp
@@ -0,0 +1,17 @@
+//
+// Created by fabio on 10/4/20.
+//
+
+#include "Ejemplo1.h"
+#include <cstdio>
+#include "opencv4/opencv2/core.hpp"
+
+
+using namespace std;
+using namespace cv;
+
+int main(){
+    
+}
+
+
---
+++ Ejemplo1.h
@@ -0,0 +1,8 @@
+//
+// Created by fabio on 10/4/20.
+//
+
+#ifndef OPENCV_UC3M_EJEMPLO1_H
+#define OPENCV_UC3M_EJEMPLO1_H
+
+#endif //OPENCV_UC3M_EJEMPLO1_H
